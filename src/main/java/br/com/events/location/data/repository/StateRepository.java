beb7e19add087717ceba47cdaea60cc08c6bd1c4
gabrielga-dev <dealmeida.gabriel.d@gmail.com>
2023-10-18 01:00:23 -0300

    changing database and ms-location functioning methods

--- src/main/java/br/com/events/location/data/repository/StateRepository.java
+++ src/main/java/br/com/events/location/data/repository/StateRepository.java
@@ -1,6 +1,5 @@
 package br.com.events.location.data.repository;
 
-import br.com.events.location.data.entity.Country;
 import br.com.events.location.data.entity.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
